c4aa22ddc5325f4c4f8598fb48b1477609aa0deb
Kristin Barr <kristinbarr@gmail.com>
2019-09-30 21:10:38 -0400

    initial project and GET route complete

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// implement your API here
+const express = require('express')
+const db = require('./data/db')
+const server = express()
+
+server.use(express.json())
+
+// GET root test
+server.get('/', (req, res) => {
+  res.send('hello world')
+})
+
+// GET users
+server.get('/api/users', (req, res) => {
+  db.find()
+    .then((users) => {
+      res.send(users)
+    })
+    .catch((error) => {
+      res.send(error)
+    })
+})
+
+const port = 8000
+server.listen(port, () => console.log(`\n** API on port ${port} ** \n`))
